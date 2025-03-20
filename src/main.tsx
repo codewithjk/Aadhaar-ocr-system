36c3acbeeec8d0da9f685558ca48c4d51743d1f4
jeevan <jeevankumar8943@gmail.com>
2025-03-20 11:04:27 +0530

    added Oauth domain and client id directly

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,8 @@ createRoot(document.getElementById('root')!).render(
   
   <StrictMode>
     <Auth0Provider
-  domain={import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN as string}
-  clientId={import.meta.env.VITE_REACT_APP_CLIENT_ID as string}
+  domain='dev-hj6sgg3wjvjv0baa.us.auth0.com'
+  clientId='JDAoZUQdCkESot2aEbaMVT7lSZrie4Zs'
       authorizationParams={{
     audience:"https://dev-hj6sgg3wjvjv0baa.us.auth0.com/api/v2/",
    redirect_uri: window.location.origin,
